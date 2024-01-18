f74aa758757d93254f8713529aeceb0e134ea55c
Max Netterberg <max.netterberg@mevisio.com>
2024-01-18 15:54:10 +0100

    disabled ts-tools

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.ts*",
-  command = ":TSToolsRemoveUnusedImports",
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.ts*",
+--   command = ":TSToolsRemoveUnusedImports",
+-- })
