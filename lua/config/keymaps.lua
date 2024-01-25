a14deedb606e83e0cd743018e4184171920cf15e
Max Netterberg <max.netterberg@mevisio.com>
2024-01-25 09:38:32 +0100

    remove autocomplete

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,8 +11,3 @@ vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
 vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
-
--- Trigger autocomplete
-vim.keymap.set("i", "<C-c>", function(context)
-  vim.lsp.buf.completion(context)
-end)
