76e86329d1b26910db649e71afada09a9e3befa7
Max Netterberg <max.netterberg@gmail.com>
2024-01-31 21:36:05 +0100

    python stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,9 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.ts*",
   command = ":TSToolsRemoveUnusedImports sync",
 })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python" },
+  callback = function()
+    require("swenv.api").auto_venv()
+  end,
+})
