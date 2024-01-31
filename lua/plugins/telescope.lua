7052001d31d7006b02da3ca86bb154dbb87749a2
Max Netterberg <max.netterberg@gmail.com>
2024-01-31 23:36:24 +0100

    added ignore patterns again

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,27 @@
 return {
   {
     "telescope.nvim",
-    config = {
-      hidden = true,
-    },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            "build",
+            "dist",
+            ".git",
+          },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+          },
+        },
+      })
+    end,
   },
 }
